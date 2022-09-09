66b19a54e0e347a20cfb66617693b3a98d84dc6b
Harout Mardirossian <harmardir@users.noreply.github.com>
2022-09-09 10:08:39 +0300

    adding new files

---
+++ js_samples/conditional_statement/conditional02.js
@@ -0,0 +1,11 @@
+var place = 'first';
+
+if(place == 'first'){
+    console.log('Gold')
+} else if (place == 'second'){
+    console.log('Silver')
+} else if (place == 'third'){
+    console.log('bronze')
+} else {
+    console.log('No medal')
+}
---
+++ js_samples/conditional_statement/conditional03.js
@@ -0,0 +1,15 @@
+var place = 'first';
+
+switch(place) {
+    case 'first':
+        console.log('Gold');
+        break;
+    case 'first':
+        console.log('Gold');
+        break;
+    case 'first':
+        console.log('Gold');
+        break;
+    default:
+        console.log('No medal');
+}
